164365ab7884871e3fcf52231ee4bd3cc9f9037f
mzembron <01149450@pw.edu.pl>
2022-01-15 15:35:03 +0100

    1st test

--- scripts/_test.py
+++ scripts/_test.py
@@ -13,11 +13,21 @@ def test_percentage_of_missing_values_in_data():
     data = pd.read_csv(filename_iris, skiprows=1, header=None)
     X = data.iloc[:, :-1].values
 
-    percentage = 70
+    percentage = 65
     index = 1
+    
     missing_values_creator = MissingValuesCreator(percentage)
     X_missing_values = missing_values_creator.add_missing_values_by_list(X, [index])
-    number_of_elements_missing = X_missing_values[]
+    number_of_elements_missing = 0
+    
+    # it was easier to loop than use count() method - "nan" has some stange type
+    for element in X_missing_values[:, 1].tolist():
+        if str(element) == "nan":
+            number_of_elements_missing += 1
+
+    number_of_all_elements = len(X_missing_values[:, 1].tolist())
+    assert(round(number_of_elements_missing/number_of_all_elements,2) == (percentage/100))
+
     
 
 
